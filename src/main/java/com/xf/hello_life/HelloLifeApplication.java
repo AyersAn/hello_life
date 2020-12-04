33f608fd9fb3ff4f28a10b87d54340b89dcf96eb
xiongfeng <18779993910@163.com>
2020-12-04 09:53:37 +0800

    初始化项目

---
+++ src/main/java/com/xf/hello_life/HelloLifeApplication.java
@@ -0,0 +1,13 @@
+package com.xf.hello_life;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloLifeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloLifeApplication.class, args);
+    }
+
+}
